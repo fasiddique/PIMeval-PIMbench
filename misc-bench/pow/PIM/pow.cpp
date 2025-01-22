2fd2ad61d61add22145b5e519f1d9d0fb4b26fca
fasiddique <farzana@virginia.edu>
2025-01-22 16:31:37 -0500

    exponent in progress

---
+++ misc-bench/pow/PIM/pow.cpp
@@ -0,0 +1,158 @@
+// Test: C++ version of pow (x, n)
+// Copyright (c) 2024 University of Virginia
+// This file is licensed under the MIT License.
+// See the LICENSE file in the root of this repository for more details.
+
+#include <iostream>
+#include <vector>
+#include <getopt.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <cstring>
+#include <cassert>
+#include <cinttypes>
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
+#include "../../util.h"
+#include "libpimeval.h"
+
+// Params ---------------------------------------------------------------------
+typedef struct Params
+{
+  uint64_t dataSize;
+  uint64_t n;
+  char *configFile;
+  char *inputFile;
+  bool shouldVerify;
+} Params;
+
+void usage()
+{
+  fprintf(stderr,
+          "\nUsage:  ./pow.out [options]"
+          "\n"
+          "\n    -l    vector length (default=2048 elements)"
+          "\n    -n    exponent (default=2)"
+          "\n    -c    dramsim config file"
+          "\n    -i    input file containing two vectors (default=generates vectors with random numbers)"
+          "\n    -v    t = verifies PIM output with host output. (default=false)"
+          "\n");
+}
+
+struct Params getInputParams(int argc, char **argv)
+{
+  struct Params p;
+  p.dataSize = 2048;
+  p.n = 2;
+  p.configFile = nullptr;
+  p.inputFile = nullptr;
+  p.shouldVerify = false;
+
+  int opt;
+  while ((opt = getopt(argc, argv, "h:l:n:c:i:v:")) >= 0)
+  {
+    switch (opt)
+    {
+    case 'h':
+      usage();
+      exit(0);
+      break;
+    case 'l':
+      p.dataSize = strtoull(optarg, NULL, 0);
+      break;
+    case 'n':
+      p.n = strtoull(optarg, NULL, 0);
+      break;
+    case 'c':
+      p.configFile = optarg;
+      break;
+    case 'i':
+      p.inputFile = optarg;
+      break;
+    case 'v':
+      p.shouldVerify = (*optarg == 't') ? true : false;
+      break;
+    default:
+      fprintf(stderr, "\nUnrecognized option!\n");
+      usage();
+      exit(0);
+    }
+  }
+  return p;
+}
+
+void pow(uint64_t vectorLength, uint64_t exponent, const std::vector<int> src, std::vector<int> &dst)
+{
+  PimObjId dstObj = pimAlloc(PIM_ALLOC_AUTO, vectorLength, PIM_INT32);
+  assert(dstObj != -1);
+
+  PimStatus status = pimCopyHostToDevice((void *) src.data(), dstObj);
+  assert(status == PIM_OK);
+
+  for (uint64_t i = 0; i < exponent/2; i++) {
+    status = pimMul(dstObj, dstObj, dstObj);
+    assert(status == PIM_OK);
+  }
+
+  dst.resize(vectorLength);
+  status = pimCopyDeviceToHost(dstObj, (void *) dst.data());
+  assert(status == PIM_OK);
+  
+  pimFree(dstObj);
+}
+
+int main(int argc, char *argv[])
+{
+  struct Params params = getInputParams(argc, argv);
+  std::vector<int> src, dst;
+  uint64_t vectorLength = params.dataSize;
+  
+  if (params.inputFile == nullptr)
+  {
+    src.resize(vectorLength);
+    
+    getVector(vectorLength, src);
+  }
+  else
+  {
+    std::cerr << "Reading from the input file is not implemented yet for the input matrix" << std::endl;
+    return 1;
+  }
+
+  std::printf("Performing power of %" PRIu64 " on %" PRIu64 "data points\n", params.n, vectorLength);
+
+  if (!createDevice(params.configFile))
+  {
+    return 1;
+  }
+
+  pow(vectorLength, params.n, src, dst);
+
+  if (params.shouldVerify)
+  {  
+    int errorFlag = 0;
+
+    #pragma omp parallel for 
+    for (uint64_t i = 0; i < vectorLength; ++i) 
+    {    
+      if (std::pow(src[i], params.n) != dst[i])
+      {
+        std::cout << "Wrong answer at index " << i << " | Wrong PIM answer = " << dst[i] << " (Baseline expected = " << (src[i] ^ params.n) << ")" << std::endl;
+        errorFlag = 1;
+      }
+    }
+    if (!errorFlag)
+    {
+      std::cout << "Correct!" << std::endl;
+    }
+  }
+
+  pimShowStats();
+
+  return 0;
+}
